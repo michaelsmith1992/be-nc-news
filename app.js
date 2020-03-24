e3a64407bf4f59e6f80b68b9bbd69e758d70c0d5
Michael Smith <michael_smith17@hotmail.co.uk>
2020-03-24 15:18:41 +0000

    db fix

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const apiRouter = require('./routes/api.router');
 const { formatDB } = require('./db/utils/utils');
 const errorHandler = require('./middleware/app-errors');
+
+var allowedOrigins = ['http://localhost:8080'];
+app.use(
+  cors({
+    origin: 'http://localhost:8080'
+  })
+);
 
 app.use(express.json());
 
